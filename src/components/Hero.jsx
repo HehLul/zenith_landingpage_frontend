2d99dd91cb2c5de0c0c7609f7f1b8c755a1038db
Ahmed Salman <81371023+HehLul@users.noreply.github.com>
2024-07-29 10:18:23 -0400

    Hero small screen pictures adjusted.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,17 @@
 import kobe from "../assets/Zenith/kobe.jpeg";
 import thomas from "../assets/Zenith/download.jpeg";
-import jocko from "../assets/Zenith/gogginsnew.jpeg";
+import jocko from "../assets/Zenith/miketyson.jpeg";
 import john from "../assets/Zenith/Keanu Reeves.jpeg";
-import david from "../assets/Zenith/miketyson.jpeg";
+import david from "../assets/Zenith/gogginsnew.jpeg";
 
 function Hero() {
   return (
     <div className="relative overflow-hidden h-screen bg-black text-white">
-      <div className="absolute inset-0 flex flex-col items-center justify-center z-10 px-4">
-        <h1 className="text-6xl font-semibold font-roboto font-serif max-w-3xl text-center mb-4">
-          Awaken the Giant Within Today
+      <div className="absolute inset-0 flex flex-col items-center justify-center z-10">
+        <h1 className="text-6xl font-semibold font-roboto font-serif max-w-3xl text-center">
+          Embrace the Mindset of High Achievers Effortlessly
         </h1>
-        <h2 className="text-2xl font-semibold max-w-3xl text-center mt-4">
+        <h2 className="text-2xl font-semibold max-w-3xl text-center mt-8">
           Leverage our curated tools to gain an unfair advantage amongst high
           achievers. Start your journey today and redefine your limits.
         </h2>
@@ -30,27 +30,60 @@ function Hero() {
           Enter your best email to be first in line for Zenith's launch.
         </h3>
       </div>
-      <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
-        <div className="relative w-full h-full flex">
-          <div className="absolute top-0 left-0 flex w-full h-full animate-scroll">
-            <img className="w-64 h-auto mx-4" src={kobe} alt="Kobe Bryant" />
+      <div className="absolute top-0 left-0 w-full h-full overflow-hidden flex items-center justify-center">
+        <div className="relative flex w-full h-full">
+          <div className="flex w-full h-full justify-center">
             <img
-              className="w-64 h-auto mx-4"
-              src={thomas}
-              alt="Thomas Edison"
+              className="flex-shrink-0 max-w-lg h-auto mx-4 my-4 sm:hidden block m-auto"
+              src={john}
+              alt="Jocko Willink"
             />
-            <img className="w-64 h-auto mx-4" src={jocko} alt="Jocko Willink" />
-            <img className="w-64 h-auto mx-4" src={john} alt="John Wick" />
-            <img className="w-60 h-auto mx-4" src={david} alt="David Goggins" />
-            <img className="w-64 h-auto mx-4" src={kobe} alt="Kobe Bryant" />
-            <img
-              className="w-64 h-auto mx-4"
-              src={thomas}
-              alt="Thomas Edison"
-            />
-            <img className="w-64 h-auto mx-4" src={jocko} alt="Jocko Willink" />
-            <img className="w-64 h-auto mx-4" src={john} alt="John Wick" />
-            <img className="w-60 h-auto mx-4" src={david} alt="David Goggins" />
+            <div className="hidden sm:flex md:hidden">
+              {[john, thomas, jocko, kobe, david].map((src, index) => (
+                <img
+                  key={index}
+                  className="flex-shrink-0 w-64 h-auto mx-4 opacity-75"
+                  src={src}
+                  alt={`Image ${index}`}
+                />
+              ))}
+              {[kobe, thomas, jocko, john, david].map((src, index) => (
+                <img
+                  key={index + 5}
+                  className="flex-shrink-0 w-64 h-auto mx-4"
+                  src={src}
+                  alt={`Image ${index + 5}`}
+                />
+              ))}
+            </div>
+            <div className="hidden md:flex lg:hidden justify-center">
+              {[jocko, kobe, thomas].map((src, index) => (
+                <img
+                  key={index}
+                  className="flex-shrink-0 w-64 h-auto mx-4"
+                  src={src}
+                  alt={`Image ${index}`}
+                />
+              ))}
+            </div>
+            <div className="hidden lg:flex">
+              {[john, thomas, jocko, kobe, david].map((src, index) => (
+                <img
+                  key={index}
+                  className="flex-shrink-0 w-64 h-auto mx-4 opacity-75"
+                  src={src}
+                  alt={`Image ${index}`}
+                />
+              ))}
+              {[kobe, thomas, jocko, john, david].map((src, index) => (
+                <img
+                  key={index + 5}
+                  className="flex-shrink-0 w-64 h-auto mx-4"
+                  src={src}
+                  alt={`Image ${index + 5}`}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
